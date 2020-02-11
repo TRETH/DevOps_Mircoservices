f81a135cdf3df83326a7fd1c27fcba6d35b06f84
Thomas Rogenhofer <trogenhofer@Thomass-MBP.fritz.box>
2020-02-11 17:25:50 +0100

    Configured shell script according to project task 5

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -5,14 +5,18 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=trog1/udacitypredict:v1
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run microservice --image=$dockerpath --port=80
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward deployment/microservice 8000:80
 

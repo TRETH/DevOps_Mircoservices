756c3742070c2537316a7766b07ad5a15e744573
Thomas Rogenhofer <trogenhofer@Thomass-MBP.fritz.box>
2020-02-11 17:27:06 +0100

    Configured shell script according to project task 6

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=udacitypredict
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login --username trog1
+docker tag udacitypredict trog1/udacitypredict:v1
 
 # Step 3:
 # Push image to a docker repository
+docker push trog1/udacitypredict:v1
